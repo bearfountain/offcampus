fb0e8542e9895a6a552e3c5146d3c017961a2eaf
Herb Susmann <herbps10@gmail.com>
2012-05-31 15:22:38 -0400

    My local config

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Offcampus::Application.initialize!
 
-Rails.logger = Logger.new(STDOU
+Rails.logger = Logger.new(STDOUT)
